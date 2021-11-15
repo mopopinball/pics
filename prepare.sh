3bb7d31c9756988efb88a83b83c5b5691f4ea0bf
Sebastian Gsellmann <sirvannos@hotmail.com>
2021-11-14 20:40:12 -0500

    fix: prepare script

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,6 @@
 pushd versions
 npm ci
 node update.sh
+popd
 
 tar cvzf dist.tar.gz versions/switches-version.json versions/displays-version.json versions/driver-version.json mopo-displays/dist/default/production/mopo-displays.production.hex mopo-driver/dist/default/production/mopo-driver.production.hex mopo-switches/dist/default/production/mopo-switches.production.hex
